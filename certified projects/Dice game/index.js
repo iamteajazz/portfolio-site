ce140519b733fd819b0e2f4d1c3a58cc03000d8b
Elvis Itive Ovie <117022841+iamteajazz@users.noreply.github.com>
2023-09-06 16:49:55 +0100

    Update index.js

--- certified projects/Dice game/index.js
+++ certified projects/Dice game/index.js
@@ -1,26 +1,22 @@
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+  var randomImgSrc1 = "./images/" + randomDiceImage1;
 
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+  var randomImgSrc2 = "./images/" + randomDiceImage2;
 
-var randomNumber1 = Math.floor(Math.random() * 6) +1; // random number 1-6
-var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1 - dice6.png
-var randomImgSrc = "images/" + randomDiceImage; // images/dice1.png - images/dice6.png
-var image1 = document.querySelectorAll ("img")[0]; // this selects the first src img
-image1.setAttribute ("src", randomImgSrc); 
+  document.querySelector(".img1").setAttribute("src", randomImgSrc1);
+  document.querySelector(".img2").setAttribute("src", randomImgSrc2);
 
-var randomNumber2 = Math.floor (Math.random () * 6) +1;
-var randomDiceImg2 =  "images/dice" + randomNumber2 + ".png";
-document.querySelectorAll ("img") [1].setAttribute ("src", randomDiceImg2);
-
-
-// if player 1 wins
-if (randomNumber1 > randomNumber2){
-  
-  document.querySelector ("h1").innerHTML = "🥇 player 1 won";  
-}
-else if (randomNumber2 > randomNumber1) {
-    document.querySelector ("h1").innerHTML = " player 2 won🥇";
-} 
-else { document.querySelector ("h1").innerHTML = "draw!🥇";
-
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🥇 Player 1 won!";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 won! 🥇";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw! 🎲";
+  }
 }
 
-
+document.querySelector("#rollButton").addEventListener("click", rollDice);
